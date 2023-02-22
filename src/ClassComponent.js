e5fa060a413444b7f32f810f1a4e8c0df244b15a
BhavyaNandyala <bhavyanandyala607@gmail.com>
2023-02-22 12:17:39 +0530

    first commit

---
+++ src/ClassComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class ClassComponent extends React.Component{
+	render(){
+		return(
+			<div>
+			<h2> GOOD MORNING </h2>
+			</div>
+			)
+	}
+}
+export default ClassComponent
